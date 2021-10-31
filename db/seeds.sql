06009bae993434c5bd5f405e1cf557fbd9491339
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-31 15:42:45 -0500

    seeds bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,14 +2,14 @@ INSERT INTO department (department_name)
 VALUES
   ('Finance'),
   ('Marketing'),
-  ('Management'),
+  ('Management');
   
 INSERT INTO roles (title, salary, department_id)
 VALUES
   ('CFO', 200,000.00, 1),
   ('Accounting', 120,000.00, 1),
   ('Marketing Sales', 100,000.00, 2),
-  ('Genereal Manager', 110,000.00, 3),
+  ('Genereal Manager', 110,000.00, 3);
 
 
 
@@ -17,6 +17,6 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
   ('Cherry', 'Hills',2,3),
   ('Amber', 'Frost',1,1),
-  ('Wendy', 'Thomas',2, 2 )
-  ('Crystal', 'Lands',3,2),
+  ('Wendy', 'Thomas',2, 2 ),
+  ('Crystal', 'Lands',3,2);
  
